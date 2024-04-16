82f1343d77042b682db323ac44f443b5b9c3a644
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-04-16 17:40:56 +0100

    Injected basic form

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary </header>
-      <footer>
-        {" "}
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/97316-emilia-podmore"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Emilia Podmore
-        </a>
-        , it is{" "}
-        <a
-          href="https://github.com/EmiliaPodmore/react-dictionary"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on Github
-        </a>{" "}
-        and <a href="/" target="_blank" rel="noopener noreferrer"></a>hosted on
-        Netlify
-      </footer>
+      <header>Dictionary </header>
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          {" "}
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/97316-emilia-podmore"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Emilia Podmore
+          </a>
+          , it is{" "}
+          <a
+            href="https://github.com/EmiliaPodmore/react-dictionary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>{" "}
+          and <a href="/" target="_blank" rel="noopener noreferrer"></a>hosted
+          on Netlify
+        </footer>
+      </div>
     </div>
   );
 }
