55dc5edd9ec19cab9bbc8ddc3390c441c3daf497
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-04-16 16:03:57 +0100

    Added footer with links

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <header className="App-header">Dictionary </header>
+      <footer>
+        {" "}
+        This project was coded by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/97316-emilia-podmore"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Emilia Podmore
+        </a>
+        , it is{" "}
+        <a
+          href="https://github.com/EmiliaPodmore/react-dictionary"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on Github
+        </a>{" "}
+        and <a href="/" target="_blank" rel="noopener noreferrer"></a>hosted on
+        Netlify
+      </footer>
     </div>
   );
 }
