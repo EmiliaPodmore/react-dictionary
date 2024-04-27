ecf4b47c6689b61790c54aa62f90744e0750afb0
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-04-27 14:47:41 +0100

    Edited CSS

--- src/App.js
+++ src/App.js
@@ -9,16 +9,22 @@ export default function App() {
         <header className="header">
           Dictionary
           <img
-            src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/123/698/original/bookshelf.gif?1713527161"
-            alt="Animated books"
+            src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/123/700/original/search.gif?1713527221"
+            alt="Animated cartoon book"
           ></img>
         </header>
         <main>
           <Dictionary defaultKeyword="spring" />
         </main>
         <footer>
-          {" "}
-          This project was coded by{" "}
+          Animated icons created by{" "}
+          <a
+            href="https://www.flaticon.com/free-animated-icons/ethics"
+            title="ethics animated icons"
+          >
+            Freepik - Flaticon
+          </a>
+          <br /> This project was coded by{" "}
           <a
             href="https://www.shecodes.io/graduates/97316-emilia-podmore"
             target="_blank"
@@ -40,7 +46,7 @@ export default function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            hosted on Netlify
+            hosted on Netlify{" "}
           </a>
         </footer>
       </div>
