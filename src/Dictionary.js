21c3cd49d6c9fe1881c7a5288c87568f72eada83
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-04-27 18:02:36 +0100

    Added images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,29 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Images from "./Images";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data);
+  }
+  function handleImageResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
   }
   function search() {
     //documentation: https://api.shecodes.io/dictionary/v1/define?word=book&key=97ca3fd7c1847b8c2t8cf2o00a730a3a
     let apiKey = "97ca3fd7c1847b8c2t8cf2o00a730a3a";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let imageApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+    axios.get(imageApiUrl).then(handleImageResponse);
   }
 
   function handleSubmit(event) {
@@ -36,7 +45,6 @@ export default function Dictionary(props) {
           <form onSubmit={handleSubmit}>
             <input
               type="search"
-              placeholder="Search..."
               autoFocus={true}
               onChange={handleKeywordChange}
             />
@@ -45,7 +53,9 @@ export default function Dictionary(props) {
             <small>Suggestions: Autumn, Bonsai, Conundrum...</small>
           </div>
         </div>
+        <br />
         <Results results={results} />
+        <Images photos={photos} />
       </div>
     );
   } else {
---
+++ src/Images.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Images.css";
+
+export default function Images(props) {
+  console.log(props.photos);
+  if (props.photos) {
+    return (
+      <section className="Images">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                {" "}
+                <a href={photo.src.original} target="_blank">
+                  <img src={photo.src.landscape} className="img-fluid" />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
