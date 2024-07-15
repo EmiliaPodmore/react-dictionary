2deb18afc2b610405e2758d224c111a3c53632b1
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-07-15 18:56:51 +0100

    Changed design

--- src/App.js
+++ src/App.js
@@ -6,27 +6,12 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="header">
-          Dictionary
-          <img
-            src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/124/662/original/read.gif?1714237906"
-            alt="Animated flipping through book icon"
-          ></img>
-        </header>
+        <header className="header">Dictionary</header>
         <main>
           <Dictionary defaultKeyword="spring" />
         </main>
         <footer>
-          Animated icon created by{" "}
-          <a
-            href="https://www.flaticon.com/free-animated-icons/ethics"
-            title="ethics animated icons"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Freepik - Flaticon
-          </a>
-          <br /> This project was coded by{" "}
+          This project was coded by{" "}
           <a
             href="https://www.shecodes.io/graduates/97316-emilia-podmore"
             target="_blank"
