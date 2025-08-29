ce68b99031de174493da9e6c9d4597280b4e8456
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2025-08-29 21:41:05 +0100

    Edited header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="header">Dictionary</header>
+        <header className="header">
+          <h1>
+            <span className="first">D</span>
+            <span className="second">i</span>
+            <span className="third">c</span>
+            <span className="fourth">t</span>
+            <span className="fifth">i</span>
+            <span className="first">o</span>
+            <span className="second">n</span>
+            <span className="third">a</span>
+            <span className="fourth">r</span>
+            <span className="fifth">y</span>
+          </h1>
+        </header>
         <main>
           <Dictionary defaultKeyword="spring" />
         </main>
